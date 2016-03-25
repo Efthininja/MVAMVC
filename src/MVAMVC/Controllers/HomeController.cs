2eff1e51dc73441df45d8eacebab607d9c1ccba6
Jason Efthimiou <Jason Efthimiou>
2016-03-25 03:54:47 -0400

    First Upload, Project still in early development

---
+++ src/MVAMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using MVAMVC.Models;
+
+namespace MVAMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Foo()
+        {
+            return "Returning Foo";
+        }
+
+        [HttpPost]
+        public IActionResult About(User newuser)
+        {
+            return View(newuser);
+        }
+
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This text is from HomeController.cs";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MVAMVC/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVAMVC.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(20), MinLength(2)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Confirm { get; set; }
+    }
+}
